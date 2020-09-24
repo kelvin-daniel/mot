11add6def379a2b19e44d0a7468772bc49ce6073
kelvin-daniel <kaymutor@gmail.com>
2020-09-24 16:14:01 +0300

    update app

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY=4d6c643773cf99821cfe8369de883a27
+export SECRET_KEY=@secretkey
+export MAIL_USERNAME=kaymutor@gmail.com
+export MAIL_PASSWORD=37042490
+
+python3 manage.py server
